d9498f04b9938ce53c0252a358772a517a5827c6
prifina-admin <tero@prifina.com>
2025-12-31 11:41:20 +0000

    31 Dec 2025, 13:41

---
+++ src/app/signup/page.js
@@ -0,0 +1,18 @@
+import { AbsoluteCenter, Box, Heading } from "@chakra-ui/react";
+
+export default function SignupPage() {
+  return (
+    <AbsoluteCenter>
+      <Box
+        w={"450px"}
+        mx="auto"
+        mt={10}
+        p={8}
+        borderWidth="1px"
+        borderRadius="lg"
+      >
+        <Heading mb={6}>Login</Heading>
+      </Box>
+    </AbsoluteCenter>
+  );
+}
