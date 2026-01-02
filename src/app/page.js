e81243dc2dd231bc04a603ac890f494785f1f75b
prifina-admin <tero@prifina.com>
2026-01-02 09:26:34 +0000

    2 Jan 2026, 11:26

--- src/app/page.js
+++ src/app/page.js
@@ -7,14 +7,10 @@ export default function HomePage() {
       <Heading mb={4}>Welcome</Heading>
       <Text mb={4}>
         This is a public home page. Try going to the{" "}
-        <ChakraLink as={Link} href="/about" color="blue.500">
-          About
+        <ChakraLink as={Link} href="/home" color="blue.500">
+          Home
         </ChakraLink>{" "}
         to see protected navigation in action.
-      </Text>
-      <Text fontSize="sm" color="gray.500">
-        Configure your Cognito env vars in <code>.env.local</code> and run{" "}
-        <code>npm install</code> then <code>npm run dev</code>.
       </Text>
     </Box>
   );
